ff3a9f6b9473c081f7552bd784b1f315242925a4
ahzam1 <ahzam1@ualberta.ca>
2019-10-03 08:54:55 -0600

    addition of about.js

---
+++ about.js
@@ -0,0 +1,28 @@
+let about = {
+  "name":"Ahzam Ahmad",
+  "uni":"University of Alberta",
+  "dob":"August 4th 1998",
+  "about":"",
+  "interests":"",
+  "languages":[
+    "HTML/CSS",
+    "JavaScript",
+    "C/C++",
+    "Java",
+    "Python",
+  ],
+  "tools":[
+    "React/React Native",
+    "Unity",
+    "Android",
+    "Git",
+    "Blender",
+    "Photoshop",
+    "FL Studio 20"
+  ],
+  "github":"ahzam1",
+  "leet":"ahzamahmad",
+  "linkedin":"ahzam-ahmad",
+  "email":"ahzam1@ualberta.ca",
+  "resume":"",
+};

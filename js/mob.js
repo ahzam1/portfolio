355640c6c6346c49eff1312382e2160768967506
ahzam1 <ahzam1@ualberta.ca>
2019-12-22 03:08:50 -0700

    projects loaded, need styling

--- js/mob.js
+++ js/mob.js
@@ -4,7 +4,84 @@ $(document).ready(function() {
   setInterval(rotate, 3000);
   let platform = getMobileOperatingSystem();
   setInterval(bounce, 1500);
+  genMobProjects();
 });
+
+function genMobProjects(){
+    post.forEach(res => {
+        let card = document.createElement("div");
+        let title = document.createElement("h2");
+        title.appendChild(document.createTextNode(res.title));
+
+        card.appendChild(title);
+
+        let desc = document.createElement("h4");
+        desc.appendChild(document.createTextNode('DESCRIPTION:'));
+        desc.style.margin=0;
+        card.appendChild(desc);
+
+        card.appendChild(createDesc(res.description));
+
+        card.appendChild(createBr("-10px"));
+
+        let tools = document.createElement("h4");
+        tools.appendChild(document.createTextNode('TOOLS:'));
+        tools.style.margin=0;
+        card.appendChild(tools);
+
+        card.appendChild(createTools(res.tools));
+
+        card.appendChild(createBr("15px"));
+
+        card.appendChild(createLink(res.link,res.video));
+
+        let container = document.querySelector(".projects_container");
+        card.classList.add("project_card");
+        container.appendChild(card);
+    });
+}
+
+function createDesc(text){
+    let description = document.createElement("p");
+    description.appendChild(document.createTextNode(text));
+    description.style.margin=0;
+    return description;
+  }
+  
+  function createTools(text){
+    let used = document.createElement("p");
+    used.appendChild(document.createTextNode(text.join(", ")));
+    used.style.margin=0;
+    return used;
+  }
+  
+  function createLink(text, video){
+    let link = document.createElement("button");
+  
+    if(video && video.length>0){
+      link.appendChild(document.createTextNode('VIDEO'));
+      link.onclick = function() {
+        window.open(video, '_blank');
+      };
+    } else if(text == ""){
+      link.disabled=true;
+      link.title="Code is not public.";
+      link.style.color="grey";
+      link.style.cursor="default"
+    }else{
+      link.appendChild(document.createTextNode('GITHUB'));
+      link.onclick = function() {
+        window.open(text, '_blank');
+      };
+    }
+    return link;
+  }
+  
+  function createBr(size){
+    let br = document.createElement("br");
+    br.style.marginTop=size;
+    return br;
+  }
 
 function bounce(){
     $(".scrollbar").animate({ 
